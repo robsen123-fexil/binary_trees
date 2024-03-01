0fa018104ba9d3092a39da86a12de45f6684ec5b
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 23:24:01 +0300

    16 BN

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,7 +9,15 @@
         int binary_tree_is_perfect(const binary_tree_t *tree)
              { return tree ? 
       (!tree->left && !tree->right ? 1 :
- binary_tree_height(tree) == floor(log2(binary_tree_size(tree) + 1))) : 
+ binary_tree_height(tree) == 
+        floor(log2(binary_tree_size(tree) + 1))) : 
      0;
  
-  }
+  }  int _pow_recursion(int x, int y) 
+                { return 
+      (y < 0 ? -1 : (y == 0 ? 1 : x * _pow_recursion(x, y - 1)))
+  ;
+     
+     
+      }
+
