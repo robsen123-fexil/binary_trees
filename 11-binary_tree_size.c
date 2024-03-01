a33d66a6d26dcf31c1bebc7cb6dd39332ba823e9
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 22:43:31 +0300

    mn

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,9 @@
-#include "binary_trees.h"
+   #include "binary_trees.h"
 
 /**
  * binary_tree_size -  size of a binary tree
  * @tree:  measure the size of
  *
  
- */
-      size_t binary_tree_size(const binary_tree_t *tree) { return tree ? binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1 : 0; }
+ */  size_t binary_tree_size(const binary_tree_t *tree) { return tree ? binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1 : 0; }
 
