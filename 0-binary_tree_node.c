d2a58d5d6ecc144762ba36b1cb6a247ff763e257
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-03-01 23:52:32 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,4 @@
-    #include "binary_trees.h"
+                       #include "binary_trees.h"
 
 
 /**
@@ -8,8 +8,11 @@
         
  */
  
- binary_tree_t *binary_tree_node(binary_tree_t *parent, 
-                                 int value){binary_tree_t *new; new = malloc(sizeof(binary_tree_t));if (new==NULL)return (NULL);
-	    new->n = value;new->parent = parent; new->left = NULL;new->right = NULL;
-return (new);
-}
+                                     binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) { 
+	                  binary_tree_t *new = malloc(sizeof(binary_tree_t
+		 )
+		 ); if (new == NULL
+		 ) return (NULL
+		 ); new->n = value; new->parent = parent; new->left = NULL; new->right = NULL; return (new
+				  ); 
+				     }
