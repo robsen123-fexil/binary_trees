5753e8dc1264bb9ba93c97f4a6b73a81ec0d47a6
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 19:34:21 +0300

    tree to delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,6 +6,6 @@
  */
     void binary_tree_delete(binary_tree_t *tree)
   {
- if (tree! NULL){  binary_tree_delete(tree->right); binary_tree_delete(tree->left); free(tree);}
+ if (tree!= NULL){ return ;  binary_tree_delete(tree->right); binary_tree_delete(tree->left); free(tree);}
 
    }
