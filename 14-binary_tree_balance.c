05f64c3d7dccb0685c51472f8b30ed7c0c95c623
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 23:04:20 +0300

    14 bin

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,16 @@
+    #include "binary_trees.h"
+
+   /**
+   * binary_tree_balance - the balance factor of a binary tree.
+    * @tree:  to the root node of the tree to measure the balance factor.
+ 
+   */
+        int binary_tree_balance(const binary_tree_t *tree) {
+             return tree ? binary_tree_height
+             (tree->left) - binary_tree_height
+             (tree->right) : 0; }
+
+             size_t binary_tree_height(const binary_tree_t *tree) {
+                 return tree ? 1 + ((binary_tree_height(tree->left) > binary_tree_height
+                 (tree->right)) ? binary_tree_height(tree->left) : binary_tree_height(tree->right)) : 0; }
+
