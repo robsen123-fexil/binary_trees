a12035e9eef1dcb70161bcab4f23310a2e0d165b
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 19:08:21 +0300

    binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
   if (!parent)
 	 return (NULL);
 
-   new = binary_tree_node(parent, value);
+   new = malloc(sizeof(binary_tree_t);
  if (!new)
 	return (NULL);
 
