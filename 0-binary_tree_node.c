5c9c9d43d22f38ae7c9ccc7e7d4debaa49fe97b1
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 18:23:08 +0300

    #include "binary_trees.h"
    /**
     * binary_tree_node - Creates a binary tree node.
     * @parent: A pointer to the parent of the node to create.
     * @value: The value to put in the new node.
     *
     * Return: If an error occurs - NULL.
     *         Otherwise - a pointer to the new node.
     */
    binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
    {
    binary_tree_t *new;
    
    new = malloc(sizeof(binary_tree_t));
    if (new == NULL)
    	return (NULL);
    
    new->n = value;
    new->parent = parent;
    new->left = NULL;
    new->right = NULL;
    
    return (new);
    }

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node.
- * @parent: pointer to the parent of the node to create.
- * @value: The value to put in the new node
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
  *
- * Return: Pointer to the newly created node
- *         NULL on failures
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -14,13 +14,12 @@ binary_tree_t *new;
 
 new = malloc(sizeof(binary_tree_t));
 if (new == NULL)
-return (NULL);
+	return (NULL);
 
 new->n = value;
 new->parent = parent;
 new->left = NULL;
 new->right = NULL;
 
-return (new);       
+return (new);
 }
-
