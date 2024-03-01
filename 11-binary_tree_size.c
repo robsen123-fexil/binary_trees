e20d844d9286ed2e7dd2590a058bd5aca460a141
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 22:43:11 +0300

    11 bi

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,4 +6,5 @@
  *
  
  */
-   size_t binary_tree_depth(const binary_tree_t *tree) { return tree ? binary_tree_depth(tree->parent) + 1 : 0; }
+      size_t binary_tree_size(const binary_tree_t *tree) { return tree ? binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1 : 0; }
+
