63b656fd55dbbc0f42c862127997ca27d6b55e99
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 21:05:59 +0300

    tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,4 +7,4 @@
    */
 
   void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
-{if (!tree || !func){func(tree->n);binary_tree_preorder(tree->right, func); binary_tree_preorder(tree->left, func);}}
+  {if (!tree || !func){func(tree->n);binary_tree_preorder(tree->right, func); binary_tree_preorder(tree->left, func);}}
