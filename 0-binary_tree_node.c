b3ab8776acd6f3a74230cd8fa5675a12f4da5bed
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 18:52:21 +0300

    @parent: pointer to the parent node of the node to create  * @value

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,13 +2,14 @@
 
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
+ * binary_tree_node - Create a binary tree node
+ * @parent: pointer ON parent node of the node to create
  * @value: value to put in the new node
  *
  * Return: Pointer to the newly created node
- *         NULL on failure
+ *        
  */
+ 
  binary_tree_t *binary_tree_node(binary_tree_t *parent, 
                                  int value)
 
