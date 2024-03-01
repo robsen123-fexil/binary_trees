194eb4019ebc762713c36df0d257b7d8dab21d0f
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 23:40:49 +0300

    17 bn

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,15 +1,18 @@
-     #include "binary_trees.h"
+               #include "binary_trees.h"
 
-     /**
-    * binary_tree_is_perfect - if a binary tree is perfect
-   * @tree: a pointer to the root node of the tree to check
-    *
-  
-    */
-        int _pow_recursion(int x, int y) { return (y < 0 ? -1 : (y == 0 ? 1 : x * _pow_recursion(x, y - 1))); }
-
-size_t binary_tree_size(const binary_tree_t *tree) { return tree ? binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1 : 0; }
-
-size_t binary_tree_height(const binary_tree_t *tree) { return tree ? (1 + (binary_tree_height(tree->left) > binary_tree_height(tree->right) ? binary_tree_height(tree->left) : binary_tree_height(tree->right))) : 0; }
-
-int binary_tree_is_perfect(const binary_tree_t *tree) { return tree ? (!tree->left && !tree->right ? 1 : binary_tree_height(tree) == floor(log2(binary_tree_size(tree) + 1))) : 0; }
+         /**
+         * binary_tree_sibling - ]e sibling of a
+ *                       node in a binary tree.
+         * @node: A ponode to find the sibling of.
+           *
+ 
+ */
+        binary_tree_t *binary_tree_sibling(binary_tree_t *node) {
+     return (node == NULL || node->parent == NULL
+               ) ? NULL : (
+                (
+                node->parent->left == node
+        ) ? node->parent->right : node->parent->left
+     
+        ); 
+     }
