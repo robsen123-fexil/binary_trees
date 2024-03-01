b9d85f59d293ed121f764dd837f8d1cb6a37d8e0
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 18:51:01 +0300

    @parent: pointer to the parent node of the node to create  * @value

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
@@ -8,16 +9,17 @@
  * Return: Pointer to the newly created node
  *         NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new;
+ binary_tree_t *binary_tree_node(binary_tree_t *parent, 
+                                 int value)
 
-	new = malloc(sizeof(binary_tree_t));
+{
+	   binary_tree_t *new;
+	   new = malloc(sizeof(binary_tree_t));
 	if (!new)
-		return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-	return (new);
+     		return (NULL);
+	 new->n = value;
+	 new->parent = parent;
+	 new->left = NULL;
+	 new->right = NULL;
+	 return (new);
 }
