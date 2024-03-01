bbca51a71d33fd9e1eeb6c1d73dcaf7ef055f2bd
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 22:38:28 +0300

    2 binary

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,17 +13,10 @@
    
     binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, 
     
-                        int value)
-   {
-    binary_tree_t *new;
-
-     if ( !parent )return (NULL);
-    new = binary_tree_node(parent, value);
+                        int value){ binary_tree_t *new; if ( !parent )return (NULL);new = binary_tree_node(parent, value);
     if (!new)  return (NULL);
-    if (!parent->right )
-{
-	new->right = parent->right;parent->right->parent = new;
-}
+    if (!parent->right ) {new->right = parent->right;parent->right->parent = new;
+             }
 parent->right = new;   return (new);
 
 }
