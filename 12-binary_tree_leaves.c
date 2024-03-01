70da244d6ab95a47e108da7f68573ebd28d26a5a
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 22:54:49 +0300

    12 binary

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,13 @@
+  #include "binary_trees.h"
+
+  /**
+    * binary_tree_leaves - counts the leaves in a binary tree
+    * @tree: tree to count the leaves from
+    *
+    * Return: number of leaves
+  *         0 if tree is NULL
+ */
+       size_t binary_tree_leaves(const binary_tree_t *tree) { 
+           return tree ?
+         (tree->left || tree->right ? binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) : 1) : 0; 
+         }
