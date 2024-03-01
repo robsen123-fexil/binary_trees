148b40aa2e66a7d5dea11db6457d1f2faf10e59d
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 20:16:19 +0300

    return ((!node || node->left || node->right) ? 0 : 1);

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
+ * @node: A pointer to the node to check.
+ * Return: 1 if node is a leaf
+ *         0 if not a leaf
+ *         0 if node is NULL
+ */
+int binary_tree_is_leaf(const binary_tree_t *node){return ((!node || node->left || node->right) ? 0 : 1);}
