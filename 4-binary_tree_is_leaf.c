9ee6e87f064f6b78b165c20755216cca00d2beb1
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 20:25:42 +0300

    HV

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,5 +7,6 @@
  *         0 if not a leaf
  *         0 if node is NULL
  */
-int binary_tree_is_leaf(const binary_tree_t *node){return ((node==NULL || node->left || node->right) 
-       ? 0 : 1);}
+    int binary_tree_is_leaf(const binary_tree_t *node){return ((node==NULL || node->left || node->right) 
+          ? 0 : 1);
+    }
