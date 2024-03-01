e246b1d36222262f2e48a4af094ebcf831ab456f
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 18:20:57 +0300

    return (new); }

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ * @parent: pointer to the parent of the node to create.
+ * @value: The value to put in the new node
+ *
+ * Return: Pointer to the newly created node
+ *         NULL on failures
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+    new = malloc(sizeof(binary_tree_t));
+    if (new == NULL)
+	    return (NULL);
+
+        new->n = value;
+        new->parent = parent;
+        new->left = NULL;
+        new->right = NULL;
+
+        return (new);       
+}
+
