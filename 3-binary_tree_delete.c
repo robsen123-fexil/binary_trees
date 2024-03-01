01c4b556850ca2f11f3f04eaec08c04614a1755c
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-01 19:30:30 +0300

    tree to delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - eletes a binary tree.
+ * @tree: tree to delete
+ */
+    void binary_tree_delete(binary_tree_t *tree)
+  {
+ if (!tree){  binary_tree_delete(tree->right); binary_tree_delete(tree->tree);}
+
+   }
